c983e4ef52b62c4a02c7517207c3cc5be6d7f65e
Maxime Quoilin <max.quoilin@gmail.com>
2019-09-26 14:04:47 +0200

    creation of folders and correction of dependencies in pom

---
+++ gamify/src/main/java/be/afelio/mqu/gamify/persistence/ApplicationRepository.java
@@ -0,0 +1,5 @@
+package be.afelio.mqu.gamify.persistence;
+
+public class ApplicationRepository {
+
+}
---
+++ gamify/src/main/java/be/afelio/mqu/gamify/persistence/entities/AlbumEntities.java
@@ -0,0 +1,8 @@
+package be.afelio.mqu.gamify.persistence.entities;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+public class AlbumEntities {
+
+}
