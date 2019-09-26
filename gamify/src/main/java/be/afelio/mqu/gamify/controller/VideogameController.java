58281d7428f7bdd8d3ed5309eb92b22ec57da091
Maxime Quoilin <max.quoilin@gmail.com>
2019-09-26 15:22:45 +0200

    test for cors policy

--- gamify/src/main/java/be/afelio/mqu/gamify/controller/VideogameController.java
+++ gamify/src/main/java/be/afelio/mqu/gamify/controller/VideogameController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,10 +17,12 @@ import be.afelio.mqu.gamify.persistence.ApplicationRepository;
 
 
 @Controller
+
 @RequestMapping(value="videogame")
 public class VideogameController {
 	@Autowired ApplicationRepository repository;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(value="all", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ResponseDto<List<VideogameDto>>> findAll() {
 		
