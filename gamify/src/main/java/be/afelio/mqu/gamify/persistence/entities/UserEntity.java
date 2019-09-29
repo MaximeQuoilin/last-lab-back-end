9c070465daac572a261e67b5df32a17598c6b43c
Maxime Quoilin <max.quoilin@gmail.com>
2019-09-29 16:40:14 +0200

    creation of userentity
    not a lot of test

--- gamify/src/main/java/be/afelio/mqu/gamify/persistence/entities/UserEntity.java
+++ gamify/src/main/java/be/afelio/mqu/gamify/persistence/entities/UserEntity.java
@@ -1,5 +1,34 @@
 package be.afelio.mqu.gamify.persistence.entities;
 
-public class UserEntity {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class UserEntity {
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@ManyToMany
+	@JoinTable(
+			name="owns",
+			joinColumns=@JoinColumn(name="user_id"),
+			inverseJoinColumns=@JoinColumn(name="videogame_id")
+			)
+	private List<VideogameEntity> videogames;
+	
 }
